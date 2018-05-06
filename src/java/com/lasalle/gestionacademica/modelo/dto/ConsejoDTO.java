86ecd56af349c7214f4ba7688fb852b79f7ca2a1
Carolina Díaz Gómez <astridc.diaz@udea.edu.co>
2018-05-05 21:06:04 -0500

    modelo dto

---
+++ src/java/com/lasalle/gestionacademica/modelo/dto/ConsejoDTO.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.lasalle.gestionacademica.modelo.dto;
+
+/**
+ *
+ * @author Carolina
+ */
+public class ConsejoDTO {
+    
+    private String nombreConsejo;
+    private String idConsejo;
+
+    public ConsejoDTO(String nombreConsejo, String idConsejo) {
+        this.nombreConsejo = nombreConsejo;
+        this.idConsejo = idConsejo;
+    }
+
+    public String getNombreConsejo() {
+        return nombreConsejo;
+    }
+
+    public void setNombreConsejo(String nombreConsejo) {
+        this.nombreConsejo = nombreConsejo;
+    }
+
+    public String getIdConsejo() {
+        return idConsejo;
+    }
+
+    public void setIdConsejo(String idConsejo) {
+        this.idConsejo = idConsejo;
+    }
+
+    @Override
+    public String toString() {
+        return "ConsejoDTO{" + "nombreConsejo=" + nombreConsejo + ", idConsejo=" + idConsejo + '}';
+    }
+    
+}
