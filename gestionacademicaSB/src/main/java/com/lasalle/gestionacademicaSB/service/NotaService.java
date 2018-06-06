751165055456bb00a37985580e22c6f1ffc0f311
Alexis Herrera <alexizher@gmail.com>
2018-06-06 10:53:11 -0500

    Cambio en notaService

--- gestionacademicaSB/src/main/java/com/lasalle/gestionacademicaSB/service/NotaService.java
+++ gestionacademicaSB/src/main/java/com/lasalle/gestionacademicaSB/service/NotaService.java
@@ -24,5 +24,50 @@ import java.util.stream.Collectors;
  */
 public class NotaService {
    
+   private static final Logger LOGGER = LoggerFactory.getLogger(NotaService.class);
+   private NotaRepository notaRepository;
+   
+   @Autowired
+   private ModelMapper modelMapper;
+   
+   public NotaService(NotaRepository notaRepository){
+	  this.notaRepository = notaRepository;
+   }
+   
+   public NotaDto create(NotaDto notaToCreateDto) {
+	  LOGGER.debug("Inicia creación: notaToCreateDto={}", notaToCreateDto);
+
+	  Nota notaToCreate = modelMapper.map(notaToCreateDto, Nota.class);
+	  Nota result = notaRepository.save(notaToCreate);
+	  NotaDto resultDTO = modelMapper.map(result, NotaDto.class);
+
+	  LOGGER.debug("Finaiza creación: resultDTO={}", resultDTO);
+	  return resultDTO;
+   }
+   
+   public List<NotaDto> findAll() {
+        List<Nota> listNotes = (List<Nota>) notaRepository.findAll();
+        return listNotes.stream()
+                .map(nota -> modelMapper.map(nota, NotaDto.class))
+                .collect(Collectors.toList());
+    }
+
+   public NotaDto findById(Long id) {
+	   Optional<Nota> findNotaOptional = notaRepository.findById(id);
+	   Nota findNota = findNotaOptional.orElseThrow(EntityNotFoundException::new);
+	   return modelMapper.map(findNota, NotaDto.class);
+   }
+
+   public void delete(Long id) {
+	   NotaDto notaInDb = findById(id);
+	   Nota notaToDelete = modelMapper.map(notaInDb, Nota.class);
+	   notaRepository.delete(notaToDelete);
+   }
+
+   public NotaDto update(NotaDto notaDto) {
+	   findById(notaDto.getId());
+	   return create(notaDto);
+   }
+   
    
 }
